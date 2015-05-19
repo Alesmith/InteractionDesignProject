823ea0d3c6ef938ea0b0057f5e50517d3bab525f
gabriellathoren <gabriella_thoren@hotmail.com>
2015-05-19 23:44:36 +0200

    Functions

---
+++ assets/app/print.js
@@ -0,0 +1,3 @@
+function printOut() {
+    window.print();
+}
--- assets/app/tooltip.js
+++ assets/app/tooltip.js
@@ -1,3 +1,11 @@
 $(document).tooltip({
     selector: '.avi-info'
 });
+
+$(document).tooltip({
+    selector: '.print-out'
+});
+
+$(document).tooltip({
+    selector: '.avi-info'
+});
