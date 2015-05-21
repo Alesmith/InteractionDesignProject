8e140be44964ff64bfbea04e77c864e8553c84db
KytioisaCat <joel.ronnqvist@gmail.com>
2015-05-21 11:20:19 +0200

    Fidddled with links to exams while not true

--- assets/app/controller/ShowAllExamsController.js
+++ assets/app/controller/ShowAllExamsController.js
@@ -1,27 +1,42 @@
 angular
-    .module('alesmith')
-    .controller('ShowAllExamsController', ShowAllExamsController);
+  .module('alesmith')
+  .controller('ShowAllExamsController', ShowAllExamsController);
 
 ShowAllExamsController.$inject = ['$scope', '$sails', '$rootScope', '$timeout'];
 
 /* @ngInject */
 function ShowAllExamsController($scope, $sails, $rootScope, $timeout) {
-    /* jshint validthis: true */
-    var vm = this;
-    vm.title = 'ShowAllExamsController';
-    $scope.exams = [];
+  /* jshint validthis: true */
+  var vm = this;
+  vm.title = 'ShowAllExamsController';
+  $scope.exams = [];
 
-    ////////////////
+  ///////////////
 
+  refresh();
+
+  $sails.on("writtenexam", function (message) {
+    refresh();
+  });
+
+  $scope.isPublished = function(exam){
+    return !!exam.grade;
+  }
+
+  function refresh() {
     $sails.get("/v1/writtenExam?writtenBy=" + $rootScope.user.id).success(function (data) {
-        console.log($rootScope.user);
-        $scope.exams = data;
+      $scope.exams = data;
 
-    });
+      for (var key in $scope.exams){
+        var exam = $scope.exams[key];
+        exam.isPublished = function(){
+          console.log(exam.grade);
+          return !!exam.grade;
+        }
+      }
 
-    $sails.on("writtenexam", function (message) {
-        $sails.get("/v1/writtenExam?writtenBy=" + $rootScope.user.id).success(function (data) {
-            $scope.exams = data;
-        });
+      console.log(data);
     });
+  }
+
 }
