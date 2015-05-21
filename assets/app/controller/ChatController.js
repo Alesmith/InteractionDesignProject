a46e27da16dd9e275e428f14ef4d741fd1fc3733
Aleksandar Faraj <aleksandarfarajgmail.com>
2015-05-21 19:00:05 +0200

    Added chat functionality

--- assets/app/controller/ChatController.js
+++ assets/app/controller/ChatController.js
@@ -4,41 +4,50 @@
 
 angular.module("alesmith").controller("ChatController", ChatController);
 
-ChatController.$inject = ["$scope", "$sails", "$rootScope"]
+ChatController.$inject = ["$scope", "$sails", "$rootScope", "$location", "$anchorScroll", '$timeout'];
 
-function ChatController($scope, $sails, $rootScope) {
+function ChatController($scope, $sails, $rootScope, $location, $anchorScroll, $timeout) {
 
-  refresh();
-  $scope.currentChat = 0;
-
-  $scope.setCurrentChat = function (index) {
-    $scope.currentChat = index;
-  }
-
-  $scope.sendMessage = function (message) {
-    if ($scope.currentChat !== null) {
-      $scope.exams[$scope.currentChat].chat = $scope.exams[$scope.currentChat].chat || [];
-      $scope.exams[$scope.currentChat].chat.push({
-        data: message,
-        fromUs: true
-      });
-      update();
-    }
-  };
-  function update(){
-    var exam = $scope.exams[$scope.currentChat];
-    $sails.put('/v1/writtenExam/'+exam.id,exam).success();
-  }
-  $sails.on("writtenexam", function (message) {
     refresh();
-  });
+    $scope.currentChat = 0;
 
-  function refresh() {
-    $sails.get("/v1/writtenExam?writtenBy=" + $rootScope.user.id).success(function (data) {
-      $scope.exams = data;
+    $scope.setCurrentChat = function (index) {
+        $scope.currentChat = index;
+    }
 
-      console.log(data);
+    $scope.sendMessage = function (message) {
+        if ($scope.currentChat !== null) {
+            $scope.exams[$scope.currentChat].chat = $scope.exams[$scope.currentChat].chat || [];
+            $scope.exams[$scope.currentChat].chat.push({
+                data: message,
+                fromUs: true
+            });
+            update();
+        }
+    };
+    function update() {
+        var exam = $scope.exams[$scope.currentChat];
+        $sails.put('/v1/writtenExam/' + exam.id, exam).success(function () {
+            $timeout(function () {
+                $location.hash('bottom');
+                $anchorScroll();
+            }, 100);
+        });
+    }
+
+    $sails.on("writtenexam", function (message) {
+        refresh();
     });
-  }
+
+    function refresh() {
+        $sails.get("/v1/writtenExam?writtenBy=" + $rootScope.user.id).success(function (data) {
+            $scope.exams = data;
+            $timeout(function () {
+                $location.hash('bottom');
+                $anchorScroll();
+            }, 500);
+            console.log(data);
+        });
+    }
 
 }
